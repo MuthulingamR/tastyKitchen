cf736affb5e5c70c712600245d36450cbd590724
MuthulingamR <muthulingam1410@gmail.com>
2023-08-27 08:52:24 +0000

    Rename title

--- src/App.js
+++ src/App.js
@@ -9,19 +9,23 @@ import NotFound from './components/NotFound'
 
 import './App.css'
 
-const App = () => (
-  <Switch>
-    <Route exact path="/login" component={LoginForm} />
-    <ProtectedRoute exact path="/" component={Home} />
-    <ProtectedRoute exact path="/cart" component={Cart} />
-    <ProtectedRoute
-      exact
-      path="/restaurant/:id"
-      component={RestaurantsDetails}
-    />
-    <Route path="/not-found" component={NotFound} />
-    <Redirect to="/not-found" />
-  </Switch>
-)
+const App = () => {
+  document.title = 'Tasty Kitchen'
+
+  return (
+    <Switch>
+      <Route exact path="/login" component={LoginForm} />
+      <ProtectedRoute exact path="/" component={Home} />
+      <ProtectedRoute exact path="/cart" component={Cart} />
+      <ProtectedRoute
+        exact
+        path="/restaurant/:id"
+        component={RestaurantsDetails}
+      />
+      <Route path="/not-found" component={NotFound} />
+      <Redirect to="/not-found" />
+    </Switch>
+  )
+}
 
 export default App
